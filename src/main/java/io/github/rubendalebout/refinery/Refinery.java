d8a6ea5786dbea91385c82388989bbbef3342203
Ruben Dalebout <rubendalebout@gmail.com>
2024-03-06 14:53:56 +0100

    Create Refinery.java

---
+++ src/main/java/io/github/rubendalebout/refinery/Refinery.java
@@ -0,0 +1,13 @@
+package io.github.rubendalebout.refinery;
+
+public final class Refinery extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
